f66129d19c4aff116a3d8288ba5540d2c5a2b314
Igor Costa dos Santos <santoscigor@gmail.com>
2021-06-27 16:21:18 -0300

    feat: account login and token functions

--- lib/extransferapi/auth.ex
+++ lib/extransferapi/auth.ex
@@ -7,6 +7,18 @@ defmodule Extransferapi.Auth do
   alias Extransferapi.Repo
   alias Extransferapi.Auth.{Account, AccountToken}
 
+  def get_account_by_cpf(cpf) when is_binary(cpf) do
+    Repo.get_by(Account, cpf: cpf)
+  end
+
+  def get_account_by_cpf_and_password(cpf, password)
+      when is_binary(cpf) and is_binary(password) do
+    account = Repo.get_by(Account, cpf: cpf)
+    if Account.valid_password?(account, password), do: account
+  end
+
+  def get_account!(id), do: Repo.get!(Account, id)
+
   def register_account(attrs) do
     %Account{}
     |> Account.registration_changeset(attrs)
@@ -19,4 +31,23 @@ defmodule Extransferapi.Auth do
     |> Repo.insert!()
   end
 
+ def change_account_registration(%Account{} = account, attrs \\ %{}) do
+    Account.registration_changeset(account, attrs, hash_password: false)
+  end
+
+  def generate_account_session_token(account) do
+    {token, account_token} = AccountToken.build_session_token(account)
+    Repo.insert!(account_token)
+    token
+  end
+
+  def get_account_by_session_token(token) do
+    {:ok, query} = AccountToken.verify_session_token_query(token)
+    Repo.one(query)
+  end
+
+  def delete_session_token(token) do
+    Repo.delete_all(AccountToken.token_and_context_query(token, "session"))
+    :ok
+  end
 end
--- lib/extransferapi/auth/account.ex
+++ lib/extransferapi/auth/account.ex
@@ -54,4 +54,22 @@ defmodule Extransferapi.Auth.Account do
     end
   end
 
+  def valid_password?(%Extransferapi.Auth.Account{hashed_password: hashed_password}, password)
+      when is_binary(hashed_password) and byte_size(password) > 0 do
+    Bcrypt.verify_pass(password, hashed_password)
+  end
+
+  # prevents from timing attacks
+  def valid_password?(_, _) do
+    Bcrypt.no_user_verify()
+    false
+  end
+
+ def validate_current_password(changeset, password) do
+    if valid_password?(changeset.data, password) do
+      changeset
+    else
+      add_error(changeset, :current_password, "is not valid")
+    end
+  end
 end
