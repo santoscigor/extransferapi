e0c1fbec2200f2b4a4040cada33dbf87b03e0e42
Igor Costa dos Santos <santoscigor@gmail.com>
2021-06-27 16:24:00 -0300

    feat: auth controller and view

---
+++ lib/extransferapi_web/controllers/auth_controller.ex
@@ -0,0 +1,29 @@
+defmodule ExtransferapiWeb.AuthController do
+  use ExtransferapiWeb, :controller
+
+  alias Extransferapi.Auth
+  alias Extransferapi.Repo
+  import ExtransferapiWeb.Auth
+
+  action_fallback ExtransferapiWeb.FallbackController
+
+  plug :require_guest_account when action in [:login, :register]
+
+  def login(conn, %{"cpf" => cpf, "password" => password}) do
+    if account = Auth.get_account_by_cpf_and_password(cpf, password) do
+      token = get_token(account)
+      render(conn, "login.json", account: account, token: token)
+    else
+      {:error, :bad_request, "Invalid cpf or password."}
+    end
+  end
+
+  def register(conn, %{"account" => params}) do
+    with {:ok, account} <- Auth.register_account(params) do
+
+      conn
+      |> put_status(201)
+      |> render("register.json", account: account)
+    end
+  end
+end
---
+++ lib/extransferapi_web/views/auth_view.ex
@@ -0,0 +1,29 @@
+defmodule ExtransferapiWeb.AuthView do
+  use ExtransferapiWeb, :view
+  alias ExtransferapiWeb.AuthView
+
+  def render("login.json", %{:account => account, :token => token}) do
+    %{
+      data: %{
+        account: render_one(account, AuthView, "privileged_user.json", as: :account)
+      },
+      token: token
+    }
+  end
+
+  def render("register.json", %{:account => account}) do
+    %{
+      data: render_one(account, AuthView, "privileged_user.json", as: :account)
+    }
+  end
+
+  def render("privileged_user.json", %{account: account}) do
+    %{
+      id: account.id,
+      balance: account.balance,
+      firstname: account.firstname,
+      cpf: account.cpf,
+      lastname: account.lastname,
+    }
+  end
+end
