811b9615c67ceef96f417eab2d7de18a79b1f7dd
Igor Costa dos Santos <santoscigor@gmail.com>
2021-06-27 16:26:00 -0300

    refactor: accounts and token migration

--- priv/repo/migrations/20210627005159_create_accounts_table.exs
+++ priv/repo/migrations/20210627005159_create_accounts_table.exs
@@ -6,12 +6,24 @@ defmodule Extransferapi.Repo.Migrations.CreateAccountsTable do
       add :firstname, :string
       add :lastname, :string
       add :cpf, :string
-      add :password, :string
+      add :hashed_password, :string
       add :balance, :decimal,  precision: 11, scale: 2
 
       timestamps()
     end
 
     create unique_index(:accounts, [:cpf])
+
+    create table(:accounts_tokens) do
+      add :account_id, references(:accounts, on_delete: :delete_all), null: false
+      add :token, :binary, null: false
+      add :context, :string, null: false
+      add :sent_to, :string
+      timestamps(updated_at: false)
+    end
+
+    create index(:accounts_tokens, [:account_id])
+    create unique_index(:accounts_tokens, [:context, :token])
+
   end
 end
