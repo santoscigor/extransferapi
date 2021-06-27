4d257cc486e4f3ca4a93f750cf2b57798d0af4d3
Igor Costa dos Santos <santoscigor@gmail.com>
2021-06-27 19:50:06 -0300

    feat: get balance view

---
+++ lib/extransferapi_web/views/account_view.ex
@@ -0,0 +1,16 @@
+defmodule ExtransferapiWeb.AccountView do
+  use ExtransferapiWeb, :view
+  alias ExtransferapiWeb.AccountView
+
+  def render("get_balance.json", %{:account => account}) do
+    %{
+      data: render_one(account, AccountView, "account_balance.json", as: :account)
+    }
+  end
+
+  def render("account_balance.json", %{account: account}) do
+    %{
+      balance: account.balance,
+    }
+  end
+end
